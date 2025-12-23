5104f656aa93c2f376efbb469f6eec65d055eeab
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-12-23 14:05:17 -0300

    Fix type errors on `render`

--- front/lib/render.ts
+++ front/lib/render.ts
@@ -5,7 +5,7 @@ export const render = (
   data: Record<string, string>,
 ): Promise<string> =>
   new Promise((resolve, reject) => {
-    ejs.renderFile(path.pathname, data, (error, html) => {
+    ejs.renderFile(path.pathname, data, (error: Error | null, html?: string) => {
       if (error || !html) {
         reject(error ?? new Error('Template render failed'))
         return
