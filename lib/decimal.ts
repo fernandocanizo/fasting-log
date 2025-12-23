6fe6d53f1a3b6c5ab9a184f73b7819346d335d52
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-12-22 22:03:10 -0300

    Start library, add helper function to round minutes

---
+++ lib/decimal.ts
@@ -0,0 +1,2 @@
+export const decimal = (n: string | null | undefined): number =>
+  parseInt(String(n), 10)
---
+++ lib/rounder.ts
@@ -0,0 +1,32 @@
+import { decimal } from "./decimal.ts"
+import { prependZero } from "./prependZero.ts"
+
+type Direction = "up" | "down"
+
+export const round = (time: string, direction: Direction = "up"): string => {
+  const [hourStr, minuteStr] = time.split(':')
+
+  const minutes = decimal(minuteStr)
+
+  if (minutes % 5 === 0) {
+    // no rounding needed
+    return time
+  }
+
+  const steps = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 ]
+  let i 
+
+  if (direction === "up") {
+    i = 0
+    while (minutes > steps[i]) {
+      i++
+    }
+  } else {
+    i = steps.length - 1
+    while ( minutes < steps[i]) {
+      i++
+    }
+  }
+
+  return `${hourStr}:${prependZero(steps[i])}`
+}
